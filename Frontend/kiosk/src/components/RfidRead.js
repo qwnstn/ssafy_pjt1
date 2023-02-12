3dc6081656cbe0deb6543993de53ff3edb75eea6
rudtn0820 <rudtn0820@naver.com>
2023-02-12 17:32:57 +0900

    rfid요청추가

--- Frontend/kiosk/src/components/RfidRead.js
+++ Frontend/kiosk/src/components/RfidRead.js
@@ -7,6 +7,7 @@ import Avatar from "@mui/material/Avatar";
 import LinearProgress from "@mui/material/LinearProgress";
 import { useNavigate } from "react-router-dom";
 import Nav from "./Nav";
+import axios from "axios";
 
 const theme = createTheme();
 
@@ -41,6 +42,21 @@ export default function ResultPayment() {
   if (redirect) {
     navigate(countdown > 0 ? "/kiosk/itemlist" : "/kiosk");
   }
+
+  useEffect(() => {
+    (async () => {
+      console.log("통신");
+      await axios
+        .get("http://localhost:8888/api/kiosk/rfid")
+        .then((res) => {
+          console.log("성공");
+        })
+        .catch((error) => {
+          console.log("RFID 리더 요청 에러");
+        });
+    })();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <ThemeProvider theme={theme}>
