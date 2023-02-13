3448bf637110bcde9472db38a873ea4a9cc6842b
강동훈 <asddiin@gmail.com>
2023-02-13 16:37:48 +0900

    select 쿼리문 수정 3

--- Hardware/FastAPIwithDB/crud/crud.py
+++ Hardware/FastAPIwithDB/crud/crud.py
@@ -9,7 +9,7 @@ def select_products_with_rfid(rfids: list, db: Session):
     # rlt = db.query(Product_Kiosk).filter(Product_Kiosk.rfid.in_(rfids)).all()
     rlt = list()
     for rid in rfids:
-        q = db.query(Product_Kiosk).filter(Product_Kiosk.rfid==rid)
+        q = db.query(Product_Kiosk).filter(Product_Kiosk.rfid==rid).first()
         print(q)
         rlt.append(q)
     return rlt
