3e1d90870cd2c837a5507f78bb762201bb33042c
강동훈 <asddiin@gmail.com>
2023-02-15 15:18:33 +0900

    시리얼 코드 수정

--- Hardware/FastAPIwithDB/functions/serial_test.py
+++ Hardware/FastAPIwithDB/functions/serial_test.py
@@ -9,15 +9,15 @@ from core.config import SERIAL_PORT
 class RFID_Serial_Trans:
     PORT = SERIAL_PORT                               # 포트 번호
     baud = 9600                                 # 보드레이트
+    ser = serial.Serial(PORT, baud, timeout=0.1)  # serial 통신 세팅
+    tag_uid = dict()
+    read_flag = True
     # baud = 115200                             # RFID 보드레이트 115200 기본값
     # def __enter__(self):
-    #     self.ser = serial.Serial(self.PORT, self.baud, timeout=0.1)  # serial 통신 세팅
-    #     self.tag_uid = dict()
-    #     self.read_flag = True
     #     return self
 
     async def main(self):
-        read_flag = True
+        self.read_flag = True
         self.tag_uid = dict()
         if not self.ser.is_open:
             self.ser.open()
