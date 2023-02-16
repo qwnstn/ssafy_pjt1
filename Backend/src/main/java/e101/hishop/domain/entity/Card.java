f216cadeee06ced52d981735eb9ab3f1825cf104
조용재 <116500532+Eleisia@users.noreply.github.com>
2023-02-16 14:38:47 +0900

    tostring 제거

--- Backend/src/main/java/e101/hishop/domain/entity/Card.java
+++ Backend/src/main/java/e101/hishop/domain/entity/Card.java
@@ -4,7 +4,6 @@ import lombok.*;
 
 import javax.persistence.*;
 
-@ToString(exclude = {"user, cardCategory"})
 @Entity
 @Getter
 @Setter
--- Backend/src/main/java/e101/hishop/domain/entity/CardCategory.java
+++ Backend/src/main/java/e101/hishop/domain/entity/CardCategory.java
@@ -8,7 +8,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Builder
-@ToString(exclude = {"user, pays"})
 @Entity
 @Getter
 @Setter
